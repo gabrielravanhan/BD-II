ee0df19450111500cfb1a8bffa3be03224d70d1a
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-11-30 22:34:08 -0300

    Update atividade.sql

--- Atividade BDD - Duplicação e Fragmentação/atividade.sql
+++ Atividade BDD - Duplicação e Fragmentação/atividade.sql
@@ -1,63 +1,103 @@
 -- CRIAÇÃO PARA REPLICAÇÃO--
-CREATE DATABASE hard_rock01;
+CREATE DATABASE loja01;
 
-USE hard_rock01;
+USE loja01;
 
-CREATE TABLE bandas (
-	id_banda INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE instrumentos (
+	id_instrumento INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(191) NOT NULL
 );
 
-CREATE DATABASE hard_rock02;
+CREATE DATABASE loja02;
 
-USE hard_rock02;
+USE loja02;
 
-CREATE TABLE bandas (
-	id_banda INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE instrumentos (
+	id_instrumento INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(191) NOT NULL
 );
 
 -- REPLICAÇÃO --
-INSERT INTO hard_rock01.bandas (nome)
-	VALUES ('Aerosmith');
+INSERT INTO loja01.instrumentos (nome)
+	VALUES ('Guitarra');
 
-INSERT INTO hard_rock01.bandas (nome)
-	VALUES ('Bon Jovi');
+INSERT INTO loja01.instrumentos (nome)
+	VALUES ('Baixo');
 
-INSERT INTO hard_rock02.bandas (nome)
-	VALUES ('Aerosmith');
+INSERT INTO loja01.instrumentos (nome)
+	VALUES ('Bateria');
 
-INSERT INTO hard_rock02.bandas (nome)
-	VALUES ('Bon Jovi');
+INSERT INTO loja02.instrumentos (nome)
+	VALUES ('Guitarra');
+
+INSERT INTO loja02.instrumentos (nome)
+	VALUES ('Baixo');
+
+INSERT INTO loja02.instrumentos (nome)
+	VALUES ('Bateria');
 
 -- CRIAÇÃO PARA FRAGMENTAÇÃO--
-CREATE DATABASE heavy_metalaj;
+CREATE DATABASE bandas01;
 
-USE heavy_metalaj;
+USE bandas01;
 
-CREATE TABLE bandas (
+CREATE TABLE bandas_hard_rock (
 	id_banda INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(191) NOT NULL
 );
 
-CREATE DATABASE heavy_metalkz;
+CREATE TABLE bandas_heavy_metal (
+	id_banda INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(191) NOT NULL
+);
 
-USE heavy_metalkz;
+CREATE DATABASE bandas02;
 
-CREATE TABLE bandas (
+USE bandas02;
+
+CREATE TABLE bandas_hard_rock (
+	id_banda INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(191) NOT NULL
+);
+
+CREATE TABLE bandas_heavy_metal (
 	id_banda INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(191) NOT NULL
 );
 
 -- FRAGMENTAÇÃO --
-INSERT INTO heavy_metalaj.bandas (nome)
+INSERT INTO bandas01.bandas_hard_rock (nome)
 	VALUES ('Guns N Roses');
 
-INSERT INTO heavy_metalaj.bandas (nome)
+INSERT INTO bandas01.bandas_hard_rock (nome)
 	VALUES ('AC/DC');
 
-INSERT INTO heavy_metalkz.bandas (nome)
+INSERT INTO bandas01.bandas_hard_rock (nome)
 	VALUES ('Led Zeppelin');
 
-INSERT INTO heavy_metalkz.bandas (nome)
+INSERT INTO bandas02.bandas_heavy_metal (nome)
 	VALUES ('Metallica');
+
+INSERT INTO bandas02.bandas_heavy_metal (nome)
+	VALUES ('Iron Maiden');
+
+INSERT INTO bandas02.bandas_heavy_metal (nome)
+	VALUES ('Megadeth');
+
+INSERT INTO bandas02.bandas_hard_rock (nome)
+	VALUES ('Guns N Roses');
+
+INSERT INTO bandas02.bandas_hard_rock (nome)
+	VALUES ('AC/DC');
+
+INSERT INTO bandas02.bandas_hard_rock (nome)
+	VALUES ('Led Zeppelin');
+
+INSERT INTO bandas01.bandas_heavy_metal (nome)
+	VALUES ('Metallica');
+
+INSERT INTO bandas01.bandas_heavy_metal (nome)
+	VALUES ('Iron Maiden');
+
+INSERT INTO bandas01.bandas_heavy_metal (nome)
+	VALUES ('Megadeth');
